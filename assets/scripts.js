e2b323641c32b82eb0da6b3f020849fb605bdda8
Josh <dev@cldv.org>
2022-10-01 02:49:09 +0200

    add view-source effect

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,45 +1,79 @@
 const trigger = document.querySelector('#trigger')
 const backgroundEl = document.querySelector('#background')
 let triggerLocked = false
+let triggerFirstClick = true
+let clipPathOn = false
 let colorIndex = 0
-trigger.addEventListener('click', e => {
+const rootCss = document.querySelector(':root')
+
+let clipPathInnerSize = 0
+let clipPathOuterSize = 0
+let clipPathInnerMaxSize = parseInt(getComputedStyle(document.body).getPropertyValue('--clip-path-inner-max-size'));
+let clipPathOuterMaxSize = parseInt(getComputedStyle(document.body).getPropertyValue('--clip-path-outer-max-size'));
+
+function drawClipPath (x, y) {
+    rootCss.style.setProperty('--clippath-circle-outer', 'circle(' + clipPathOuterSize + '% at ' + x + '% ' + y + '%)');
+    rootCss.style.setProperty('--clippath-circle-inner', 'circle(' + clipPathInnerSize+ '% at ' + x + '% ' + y + '%)');
+}
+
+trigger.addEventListener('click', function clickerFunction (ev) {
+    if (triggerFirstClick) {
+        let tmpContent = document.querySelector('html').innerHTML.replace(/</g,"&lt;")
+        tmpContent = tmpContent.replace(/\n\s*\n/g, '\n')
+        tmpContent = tmpContent.replace('&lt;head>', '\n\t&lt;head>')
+        tmpContent = tmpContent.replace('\t&lt;/body>', '&lt;/body>')
+        document.querySelector('#background').innerHTML = `<pre>\n&lt;!DOCTYPE html>\n&lt;html lang="en">${tmpContent}\n&lt;/html></pre>`
+        triggerFirstClick = false
+    }
+
     if (triggerLocked) {
         return
     } else triggerLocked = true
     
-    const colors = [
-        '#111111',
-        '#254e7b',
-        '#7366A1',
-        '#a17513',
-        '#524737',
-        '#77384B',
-        '#19624c'
-    ]
+    clipPathOn = !clipPathOn
+    let x, y
+    if (!clipPathOn) {
+        let clipIntID = setInterval(() => {
+            clipPathInnerSize -= clipPathInnerMaxSize / 10
+            clipPathOuterSize -= clipPathOuterMaxSize / 10
 
-    colorIndex = colorIndex == colors.length - 1 ? 0 : colorIndex + 1
+            if (clipPathInnerSize <= 0) {
+                clipPathInnerSize = 0
+                clipPathOuterSize = 0
+                window.clearInterval(clipIntID)
+                triggerLocked = false
+                return
+            }
 
-    backgroundEl.style.background = colors[colorIndex]
+            x = parseInt(ev.pageX / window.innerWidth * 100);
+            y = parseInt(ev.pageY / window.innerHeight * 100);
+            drawClipPath(x, y)
+        }, 20)
+    } else {
+        let clipIntID = setInterval(() => {
+            clipPathInnerSize += clipPathInnerMaxSize / 10
+            clipPathOuterSize += clipPathOuterMaxSize / 10
 
-    let i = 0;
-    const rootCss = document.querySelector(':root')
-    let intervalID = setInterval(() => {        
-        backgroundEl.style.setProperty('clip-path', 'circle(' + i + '% at 50% 50%)')
-        rootCss.style.setProperty('--progress-bar', i + '%');
-        
-        if (i === 100) {
-            backgroundEl.style.setProperty('clip-path', 'circle(0% at 50% 50%)')
-            document.querySelector('body').style.background = colors[colorIndex]
-            rootCss.style.setProperty('--progress-bar', '0px');
-            window.clearInterval(intervalID)
-            triggerLocked = false
-        }
-        ++i
-    }, 20)
+            if (clipPathInnerSize >= clipPathInnerMaxSize) {
+                clipPathInnerSize = clipPathInnerMaxSize
+                clipPathOuterSize = clipPathOuterMaxSize
+                window.clearInterval(clipIntID)
+                triggerLocked = false
+                return
+            }
+
+            x = parseInt(ev.pageX / window.innerWidth * 100);
+            y = parseInt(ev.pageY / window.innerHeight * 100);
+            drawClipPath(x, y)
+        }, 20)
+    }
+
+    x = parseInt(ev.pageX / window.innerWidth * 100);
+    y = parseInt(ev.pageY / window.innerHeight * 100);
+    drawClipPath(x, y)
 })
 
 document.querySelectorAll('.typer').forEach(e => {
-    console.log(e);
     let text = e.dataset.text
     if (text === undefined) return
     let count = 0
@@ -52,4 +86,19 @@ document.querySelectorAll('.typer').forEach(e => {
         if (count == 0) e.innerHTML = '' + text[count++]
         else e.innerHTML = '' + e.innerHTML + text[count++]
     }, 100)
+})
+
+function getCoordinates (ev) {
+    let x = parseInt(ev.pageX / window.innerWidth * 100);
+    let y = parseInt(ev.pageY / window.innerHeight * 100);
+    drawClipPath(x, y)
+}
+
+document.addEventListener('touchmove', (ev) => {
+    ev = ev.touches[0]
+    getCoordinates(ev)
+})
+
+document.addEventListener('mousemove', (ev) => {
+    getCoordinates(ev)
 })
